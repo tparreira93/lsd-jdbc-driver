9670c045b367581ff03002308bef948ea33bdcea
Thales <thalesvparreira@gmail.com>
2022-04-24 15:11:06 +0100

    fix: FutureConnection interface

--- src/main/kotlin/lsd/v2/api/FutureConnection.kt
+++ src/main/kotlin/lsd/v2/api/FutureConnection.kt
@@ -3,11 +3,11 @@ package lsd.v2.api
 import java.sql.Connection
 
 interface FutureConnection: Connection {
-    fun prepareFutureStatement(sql: String?): FutureStatement
+    fun prepareFutureStatement(sql: String?): PreparedFutureStatement
 
-    fun prepareFutureStatement(sql: String?, resultSetType: Int, resultSetConcurrency: Int): FutureStatement
+    fun prepareFutureStatement(sql: String?, resultSetType: Int, resultSetConcurrency: Int): PreparedFutureStatement
 
-    fun prepareFutureStatement(sql: String?, resultSetType: Int, resultSetConcurrency: Int, resultSetHoldability: Int): FutureStatement
+    fun prepareFutureStatement(sql: String?, resultSetType: Int, resultSetConcurrency: Int, resultSetHoldability: Int): PreparedFutureStatement
 
     fun isTrue(condition: String): FutureStatementCondition
 }
